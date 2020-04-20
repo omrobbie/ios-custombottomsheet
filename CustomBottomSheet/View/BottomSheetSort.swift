6088226ac8ee3df2ee52e7acad2358a3325c17e9
omrobbie <putu.roby@yahoo.com>
2020-04-21 06:21:13 +0800

    prepare bottom sheet sort layout

--- CustomBottomSheet/View/BottomSheetSort.swift
+++ CustomBottomSheet/View/BottomSheetSort.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class BottomSheetSort: BottomSheetViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewHeight = 100
+        setupUI()
+        setupSortList()
+    }
+
+    fileprivate func setupUI() {
+        viewHeight = 300
+    }
+
+    fileprivate func setupSortList() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "SortCell", bundle: nil), forCellReuseIdentifier: "SortCell")
+    }
+}
+
+extension BottomSheetSort: UITableViewDelegate , UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let switchView = UISwitch()
+
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        cell.accessoryView = switchView
+        
+        return cell
     }
 }
---
+++ CustomBottomSheet/View/SortCell.swift
@@ -0,0 +1,13 @@
+//
+//  SortCell.swift
+//  CustomBottomSheet
+//
+//  Created by omrobbie on 21/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class SortCell: UITableViewCell {
+
+}
