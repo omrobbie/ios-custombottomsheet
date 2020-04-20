e5c206458544e2dca41d70c1ee60e2b969941798
omrobbie <putu.roby@yahoo.com>
2020-04-21 04:13:13 +0800

    separate bottom sheet for reuse later

--- CustomBottomSheet/Controller/ViewController.swift
+++ CustomBottomSheet/Controller/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var bottomSheetVC = BottomSheetViewController()
+    var bottomSheetVC = BottomSheetVC()
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ CustomBottomSheet/View/BottomSheetVC.swift
@@ -0,0 +1,16 @@
+//
+//  BottomSheetVC.swift
+//  CustomBottomSheet
+//
+//  Created by omrobbie on 21/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class BottomSheetVC: BottomSheetViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
